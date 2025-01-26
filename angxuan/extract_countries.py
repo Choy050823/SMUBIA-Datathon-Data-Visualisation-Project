9ca23eaa0df03e83239141a8f2e98d41c4fab8b2
aleeax <184836747+aleeax@users.noreply.github.com>
2025-01-26 16:49:08 +0800

    Changes to be committed: 	new file:   angxuan/cleaned_data_with_countries.csv 	modified:   angxuan/extract_countries.py 	new file:   angxuan/news_categorize.csv

--- angxuan/extract_countries.py
+++ angxuan/extract_countries.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import pycountry # pip install pycountry
 
-# import data
+
+# import data (the first 400 data - category)
+news_df = pd.read_csv("news_categorize.csv")
+news_df = news_df[:400]
+
 cleaned_data = pd.read_csv("./chloe/Cleaned_Tokenized_Data/cleaned_tokenized_data.csv")
+first_400_cleaned_data = cleaned_data[:400]
 
 # extract countries function
 def extract_countries(token):
@@ -20,5 +25,10 @@ def extract_countries(token):
 
 # add a new column called "countries" into the dataframe
 cleaned_data["countries"] = cleaned_data["lemmatized_tokens"].apply(extract_countries)
-print(cleaned_data.head())
-print("Execution done.")
+first_400_cleaned_data["countries"] = first_400_cleaned_data["lemmatized_tokens"].apply(extract_countries)
+first_400_cleaned_data["label"] = news_df["Theme"].lower()
+
+# save to csv file
+cleaned_data.to_csv("./angxuan/cleaned_data_with_countries.csv", index=False)
+first_400_cleaned_data.to_csv("./angxuan/first_400_cleaned_data.csv", index = False)
+print("Data with countries saved to angxuan Folder")
