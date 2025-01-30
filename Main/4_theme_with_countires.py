8861fef8608d630f7894ef28b0a2410a79edec36
ChoyMH23 <choymh23@gmail.com>
2025-01-30 18:03:34 +0800

    added summary into the theme with countries

--- Main/4_theme_with_countires.py
+++ Main/4_theme_with_countires.py
@@ -1,15 +1,28 @@
 import pandas as pd
 
-country = pd.read_csv("./angxuan/cleaned_data_with_countries.csv").drop(columns=["tokens","filtered_tokens"]).head(1510)
+country = pd.read_csv("./angxuan/cleaned_data_with_countries.csv").drop(columns=["cleaned_summary","tokens","filtered_tokens"]).head(1510)
+summary = pd.read_csv("./minhan/text_summary.csv").drop(columns=["cleaned_summary"]).head(1510)
 print(country.head(20))
 
 themes = pd.read_csv("./angxuan/themes.csv")
 
-country["theme"] = themes["Theme"]
-print(country.head(20))
-print(country["theme"].isnull().sum())
-country.dropna(subset=["theme"], inplace=True)
-print(country["theme"].isnull().sum())
-print(len(country))
+res = pd.DataFrame({
+    "summary": summary["summary"],
+    "theme": themes["Theme"],
+    "countries": country["countries"],
+    "lemmatized_tokens": country["lemmatized_tokens"]
+})
+# country["theme"] = themes["Theme"]
+print(res.head(20))
+print(res["theme"].isnull().sum())
+res.dropna(subset=["theme"], inplace=True)
+print(res["theme"].isnull().sum())
+print(len(res))
 
-country.to_csv("./angxuan/data_country_and_label.csv", index=False)
+res = res.replace('\n', ' ', regex=True)
+
+# Check for rows with newline characters
+problematic_rows = res[res.apply(lambda row: row.str.contains('\n').any(), axis=1)]
+print(problematic_rows)
+
+res.to_csv("./angxuan/new_data_country_and_label.csv", index=False, quoting=2, escapechar='\\')
